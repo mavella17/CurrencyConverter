4dcaff6884aa3f8e012f9139de8ffd3f87b7b4b5
Matt Avella <mattavella17@gmail.com>
2023-06-26 19:45:53 +0000

    Test Commit

--- capi_main.py
+++ capi_main.py
@@ -10,33 +10,68 @@ from sqlalchemy.sql import text as sa_text
 engine = db.create_engine('sqlite:///currency.db')
 
 
-def buildDB():
+def updateDB():
     listofCurr = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json'
     currList = requests.get(listofCurr)
     currList = currList.json().keys()
     url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"
-    for currency in currList: 
+    first = True
+    for currency in currList:
         r = requests.get(url + currency + ".json")
         rates = r.json()[currency]
-        firstColumn = {'Currency':currency}
+        firstColumn = {'Currency':currency, 'Date Updated':r.json()['date']}
         firstColumn.update(rates)
-        d = pd.DataFrame(firstColumn, index=[0]) 
-        d.to_sql('exchange', con=engine, if_exists='append', index=False)
- 
+        d = pd.DataFrame(firstColumn, index=[0])
+        if first:
+            d.to_sql('exchange', con=engine, if_exists='replace', index=False)
+            first = False
+        else:
+            d.to_sql('exchange', con=engine, if_exists='append', index=False)
+
+
 def printDB():
     with engine.connect() as connection:
         query_result = connection.execute(db.text("""SELECT * FROM
         exchange;""")).fetchall()
         print(pd.DataFrame(query_result))
 
+def getList():
+    listofCurr = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json'
+    currList = requests.get(listofCurr)
+    pprint.pprint(currList.json())
+
+def base():
+    
+
+
+
 def main():
     while True:
-        base = input("Enter Your Base Currency, or LIST to view all currencies: ")
-        if base.upper() == "LIST":
-            listofCurr = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json'
-            currList = requests.get(listofCurr)
-            pprint.pprint(currList.json())
+        print("---------")
+        print("Commands:")
+        print("---------")
+        print("Input B to input a base currency")
+        print("Input U to update the exchange rate database")
+        print("Inpuy V to view the exchange rate database")
+        print("Input Q to quit")
+        print("Input L to list all currencies and their acronyms")
 
+        base = input("Type command here: ")
+        base = base.upper()
+        if base == "L":
+            getList()
+        elif base == "Q":
+            print("Quitting!")
+            break
+        elif base == "U":
+            updateDB()
+            printDB()
+        elif base == "V":
+            printDB()
+        elif base =="B":
+            print("Placeholder")
+        else:
+            print("Invalid Command, Try Again!")
         
         
 
