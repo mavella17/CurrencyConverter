cf623064f4292ca3d1240b48ac7a17de1353bed6
Matthew Avella <mavella@gatech.edu>
2023-07-06 16:57:48 +0000

    Added registration page

--- webPage.py
+++ webPage.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)  ## add this line
@@ -12,6 +13,14 @@ def home():
 @app.route("/convert")
 def convert():
     return render_template('convert.html', subtitle='Conversion Page', text='This is the conversion page')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
